2dfbbd833bc8193fe335f8c1af54273c07bf4fa3
feoolga <kirianova_ov@mail.ru>
2023-08-28 13:41:36 +0300

    make smoother scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
-window.addEventListener('scroll', e => {
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
-})
+;(function() {
+
+    window.onload = () => {
+        // устанавливаем настройки
+        let options = {
+            root: null,
+            rootMargin: "5px",
+            threshold: 0.5,
+        };
+    
+        // создаем наблюдатель
+        const observer = new IntersectionObserver((entries, observer) => {
+            // для каждой записи-целевого элемента
+            entries.forEach(entry => {
+                // если элемент является наблюдаемым
+                if (entry.isIntersecting) {
+                    const elemAnim = entry.target
+                    // меняем класс
+                    // entry.isIntersecting ? entry.target.classList.replace('active', 'activeBack') : entry.target.classList.replace('activeBack', 'active')
+                    elemAnim.classList.add('active')
+                    // прекращаем наблюдение
+                    observer.unobserve(elemAnim)
+                }
+            })
+        }, options)
+    
+        // с помощью цикла следим за всеми классами anim на странице
+        const animations = document.querySelectorAll('.anim')
+        animations.forEach(animation => {
+            observer.observe(animation)
+        })
+    }
+})();
+;(function() {
+	window.addEventListener('scroll', e => {
+        document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+    })
+    
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content'
+    })
+})();
