fa545772e3a6f4b14e1c1a14f920c021afbb9472
feoolga <kirianova_ov@mail.ru>
2023-09-04 20:54:09 +0300

    add multi menu

--- js/script.js
+++ js/script.js
@@ -32,13 +32,57 @@
     }
 })();
 ;(function() {
-	window.addEventListener('scroll', e => {
-        document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
-    })
+	// window.addEventListener('scroll', e => {
+    //     document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+    // })
     
     gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-    ScrollSmoother.create({
-        wrapper: '.wrapper',
-        content: '.content'
-    })
+
+    if(ScrollTrigger.isTouch !== 1){
+        ScrollSmoother.create({
+            wrapper: '.wrapper',
+            content: '.content',
+            smooth: 1.2,
+            effects: true,
+        })
+
+        gsap.fromTo('.main-article', {color: '#fff'}, {
+            color: '#FF0000',
+            scrollTrigger: {
+                trigger: '.main-article',
+                start: 'top',
+                end: '800',
+                scrub: true,
+            }
+        })
+
+        let itemsLeft = gsap.utils.toArray('.gallery__left .gallery__item')
+
+        itemsLeft.forEach(element => {
+            gsap.fromTo(element, {x: -100, opacity: 0}, {
+                opacity: 1, x: 0,
+                scrollTrigger: {
+                    trigger: element,
+                    start: '-800',
+                    end: '-100',
+                    scrub: true,
+                }
+            })
+        });
+
+        let itemsRight = gsap.utils.toArray('.gallery__right .gallery__item')
+
+        itemsRight.forEach(element => {
+            gsap.fromTo(element, {x: 120, opacity: 0}, {
+                opacity: 1, x: 0,
+                scrollTrigger: {
+                    trigger: element,
+                    start: '-800',
+                    end: '-100',
+                    scrub: true,
+                }
+            })
+        });
+    }
+
 })();
